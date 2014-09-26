3d3b429a2cea0d0193786f8608d66621a8fc35f5
mmusnjak <marko.musnjak@gmail.com>
2014-09-26 10:26:00 +0200

    Completed Assignment #1

--- progfun-recfun/src/main/scala/recfun/Main.scala
+++ progfun-recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,31 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || r == c) 1 else
+   	  pascal(c-1, r-1) + pascal(c, r-1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def iterate(chars: List[Char], acc: Int): Int = {
+      if(chars.isEmpty || acc < 0) acc
+      else if (chars.head == '(') iterate(chars.tail, acc + 1)
+      else if (chars.head == ')') iterate(chars.tail, acc - 1)
+      else iterate(chars.tail, acc)
+    }
+    iterate(chars, 0) == 0
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else if (money <= 0 && !coins.isEmpty) 0
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  }
 }
